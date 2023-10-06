8848b7257a9ccb5bb1e6a62fae5a16f9254e13c5
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-06 16:57:31 -0300

    refac: implemented ngFor in the menu-bar

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,30 +1,45 @@
 import { Component } from '@angular/core';
+import { MenuOptions } from 'src/app/interfaces/menuOptions';
 
 @Component({
 	selector: 'app-menu-bar',
 	template: `
-	<div class="menu-bar__container">
-		<div class="menu-bar__logo">
-			<img
-				src="assets/ps-logo.png"
-				alt="playstation store logo"
-				width="140px"
-				height="31px"
-			/>
-		</div>
+		<div class="menu-bar__container">
+			<div class="menu-bar__logo">
+				<img
+					src="assets/ps-logo.png"
+					alt="playstation store logo"
+					width="140px"
+					height="31px"
+				/>
+			</div>
 
-		<div class="menu-bar__item">
-			<ul>
-				<li>
-					<a href="https://gisellebarbosa.github.io/portfolio/index.html"
-						>PORTFÓLIO</a
-					>
-				</li>
-				<li><a href="https://www.linkedin.com/in/gisellebarb/">LINKEDIN</a></li>
-				<li><a href="https://github.com/GiselleBarbosa">GITHUB</a></li>
-			</ul>
+			<div class="menu-bar__item" *ngFor="let option of menuOptions">
+				<ul>
+					<li>
+						<a href="{{ option.link }}">{{ option.option }}</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div> `,
+	`,
 	styleUrls: ['./menu-bar.component.scss'],
 })
-export class MenuBarComponent {}
+export class MenuBarComponent {
+	public menuOptions: MenuOptions[] = [
+		{
+			option: 'PORTFÓLIO',
+			link: 'https://gisellebarbosa.github.io/portfolio/index.html',
+		},
+
+		{
+			option: 'LINKEDIN',
+			link: 'https://www.linkedin.com/in/gisellebarb/',
+		},
+
+		{
+			option: 'GITHUB',
+			link: 'https://github.com/GiselleBarbosa',
+		},
+	];
+}
---
+++ src/app/interfaces/menuOptions.ts
@@ -0,0 +1,4 @@
+export interface MenuOptions {
+	option: string;
+	link: string;
+}
